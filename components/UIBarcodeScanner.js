f997cfc56077d1ed2ee2d651b489fb4148ebb103
alanpgs <alan@greensmedia.com>
2022-10-06 17:53:59 +0400

    modal updates

--- components/UIBarcodeScanner.js
+++ components/UIBarcodeScanner.js
@@ -4,6 +4,7 @@ import BarcodeScannerComponent from "react-qr-barcode-scanner-esm5";
 const UIBarcodeScanner = ({getDetails})=> {
 
     const [data, setData] = useState('');
+    const [disable, setDisable] = useState(false);
 
     const handleScan = (value)=> {
         setData(value)
@@ -16,12 +17,14 @@ const UIBarcodeScanner = ({getDetails})=> {
                 height={'100%'}
                 onUpdate={(err, result) => {
                     if (result){
+                        setDisable(true)
                         setData(JSON.parse(result.text))
                         getDetails(data.customerID);
                         return;
                     }
                     else setData('');
                 }}
+                stopStream={disable}
             />
 
             <div className='debugger_'>
